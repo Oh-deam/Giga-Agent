a75bc678f4aa6fae48f83f36080d5440cf57a4b3
Oh-deam <poroshin_04@inbox.ru>
2026-04-10 23:29:07 +1000

    Add function for merge tables

---
+++ agent/src/tools/join_tables.py
@@ -0,0 +1,19 @@
+import pandas as pd
+from src.schemas.tables import JoinConditions
+from src.utils.storage import Storage
+
+def merge_ables(joinconditions: JoinConditions, storage: Storage):
+    
+    df_train = storage.get_table("train.csv")
+    df_test = storage.get_table("test.csv")
+    
+    for condition in joinconditions:
+        df_for_join = storage.get_table("{condition.table_name}")
+
+        pd.merge(df_train, df_for_join, left_on="{condirion.on_col1}", right_on="{condirion.on_col2}")
+        pd.merge(df_test, df_for_join, left_on="{condirion.on_col1}", right_on="{condirion.on_col2}")
+
+
+
+
+
